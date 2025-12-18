7c667ec9a649342ec92619688cf52bb69c5a7e31
Colin Snover <github.com@zetafleet.com>
2025-12-18 16:30:49 -0600

    Avoid pessimising integer arithmetic with string operands
    This stops string-integer operands causing `Value` to convert to
    floating point unnecessarily.

--- tests/scripts/arithmetic.lua
+++ tests/scripts/arithmetic.lua
@@ -26,3 +26,19 @@ do
     local i, j = -16, 3
     assert(i // j == math.floor(i / j))
 end
+
+do
+    local two_27 = 134217729
+    local two_52 = 4503599627370496
+    local two_53 = 9007199254740992
+    local two_54_plus_2 = 18014398509481986
+    assert(two_53 + "1" ~= two_53)
+    assert(two_53 + "1" == two_53 + 1)
+    assert(two_53 + 2 - "1" ~= two_53)
+    assert(two_53 + 2 - "1" == two_53 + 2 - 1)
+    assert(two_27 * "134217729" ~= 18014398777917440)
+    assert(two_27 * "134217729" == two_27 * two_27)
+    assert(two_54_plus_2 // "2" ~= two_53)
+    assert(two_54_plus_2 // "2" == two_54_plus_2 // 2)
+    assert(two_54_plus_2 % "5" == 1)
+end
